1d993dd67618735f2cdba763f5904537c299a198
mattrickallen-cloud <matt.rick.allen@gmail.com>
2026-04-14 23:34:28 +0200

    Update app_code.py

--- app_code.py
+++ app_code.py
@@ -99,7 +99,7 @@ for year in range(year_min,year_max+1):
     mean_coord["years"].append(year)
 
 colormap = cm.LinearColormap(
-                            colors=["yellow", "green", "blue"],
+                            colors=["green", "blue"],
                             vmin=df['year'].min(),
                             vmax=df['year'].max()
                             )
